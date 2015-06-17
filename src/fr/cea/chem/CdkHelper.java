5a2cd73b1596a0dd2c00038815e8ffd6e8fa0aa1
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2015-06-17 14:37:48 +0200

    Add toString() version that takes a IMolecularFormula.

--- src/fr/cea/chem/CdkHelper.java
+++ src/fr/cea/chem/CdkHelper.java
@@ -1,7 +1,10 @@
 package fr.cea.chem;
 
+import org.openscience.cdk.config.IsotopeFactory;
+import org.openscience.cdk.config.Isotopes;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IMolecularFormula;
+import org.openscience.cdk.interfaces.IIsotope;
 import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
 import org.openscience.cdk.inchi.InChIToStructure;
 import org.openscience.cdk.inchi.InChIGeneratorFactory;
@@ -36,9 +39,32 @@ public final class CdkHelper {
 
 	public static String toString(IAtomContainer c) {
 		IMolecularFormula formula = MolecularFormulaManipulator.getMolecularFormula(c);
-		String s = MolecularFormulaManipulator.getString(formula);
-		int charge = formula.getCharge();
-		if (charge != 0) {
+		return toString(formula);
+	}
+
+	public static String toString(IMolecularFormula formula) {
+
+		String s = "";
+
+		try {
+        IsotopeFactory ifac = Isotopes.getInstance();
+
+		for (IIsotope i: formula.isotopes()) {
+			if (i.getMassNumber() != ifac.getMajorIsotope(i.getSymbol()).getMassNumber()) {
+				if (s.length() != 0)
+					s += ":";
+				s += i.getMassNumber();
+			}
+			s += i.getSymbol();
+			if (formula.getIsotopeCount(i) > 1)
+				s += formula.getIsotopeCount(i);
+		}
+        } catch (java.io.IOException e) {
+			s = MolecularFormulaManipulator.getString(formula);
+	    }
+		
+		Integer charge = formula.getCharge();
+		if (charge != null && charge != 0) {
 			char[] strcharge = new char[Math.abs(charge)];
 			java.util.Arrays.fill(strcharge, charge > 0 ? '+' : '-');
 			s = "(" + s + ")" + new String(strcharge);
